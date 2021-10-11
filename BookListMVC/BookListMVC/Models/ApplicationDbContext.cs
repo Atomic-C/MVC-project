b9914a8cba6ed2291de144d67e0a1430aae2eac3
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-11 19:35:15 +0000

    Configured ApplicationDbContext

---
+++ BookListMVC/BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext // https://forums.asp.net/t/2002296.aspx?DBContext+VS+IdentityDBContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
